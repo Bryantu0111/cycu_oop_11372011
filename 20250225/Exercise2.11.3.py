f4765aa38b0dbf1f32cddd152f177d0bef2cc0df
涂子彧 11372011 <bryantu0111@gmail.com>
2025-02-25 20:06:12 +0800

    Create Exercise2.11.3.py

---
+++ 20250225/Exercise2.11.3.py
@@ -0,0 +1,22 @@
+import math
+#part 1 
+r = 5
+volume = math.pow(5,3) * math.pi *4/3
+print('part1 volume', volume)
+
+#part2
+x = 42
+v = math.sin(x)**2 + math.cos(x)**2
+
+print('part2 =' ,v)
+
+#part3
+e_2_1 = math.e ** 2
+print(e_2_1)
+
+e_2_2 = math.pow(math.e, 2)
+print(e_2_2)
+
+x = 3.5
+e_2_3 = math.exp(x)
+print(e_2_3)
