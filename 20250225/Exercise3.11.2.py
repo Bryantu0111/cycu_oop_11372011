c81d779d576c3cdd3e547c29a5f982d6a8fee587
涂子彧 11372011 <bryantu0111@gmail.com>
2025-02-25 20:26:24 +0800

    Create Exercise3.11.2.py

---
+++ 20250225/Exercise3.11.2.py
@@ -0,0 +1,11 @@
+str1 = "Monty"
+str2 = "Python's"
+str3 = "Flying Circus"
+
+message1 = ' ' * (40-len(str1)) + str1
+message2 = ' ' * (40-len(str2)) + str2
+message3 = ' ' * (40-len(str3)) + str3
+
+print(message1)
+print(message2)
+print(message3)
